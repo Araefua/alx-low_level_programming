1eabc024bb04afa7ac7da759ba7c6cd04cf45659
Oswell Paintsil <oswell.paintsil@gmail.com>
2023-02-20 22:08:54 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #Include < stdio.h >
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 int main(void)
 {
